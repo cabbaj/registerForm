3ae01801657557c0b9a8b3519650d8bcb74e374a
cabbaj <ppvadon@gmail.com>
2025-04-18 14:49:38 +0700

    submit function

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-let changed = () => {
-  document.querySelectorAll("input[name=gender]").forEach(input => {
-    if (input.checked) console.log(input.value);
+function submitForm() {
+  let firstNameInput = document.getElementById("firstname");
+  let lastNameInput = document.getElementById("lastname");
+  let ageInput = document.getElementById("age");
+  let genderInput = document.querySelector("input[name=gender]:checked");
+  let interestsInput = document.querySelectorAll("input[name=interest]:checked");
+  let descriptionInput = document.getElementById("description");
+
+  // genders.forEach(function (g) {
+  //   if (g.checked) console.log(g.value);
+  // });
+
+  // loop every input in nodeList
+  // interests.forEach(function (i) {
+  //   if (i.checked) console.log(i.value);
+  // });
+
+  let interests = "";
+
+  interestsInput.forEach((interest, idx, array) => {
+    interests += `${interest.value}, `;
+    // console.log(`length= ${array.length}`);
+    // console.log(`index= ${idx}`);
+    // interests is string now ("a, b, c")
+    if (idx === array.length - 1) {
+      // -2 is mean remove the last 2 characters -> ", "
+      interests = interests.slice(0, -2);
+    }
   });
-};
+
+  // this is json
+  let userData = {
+    firstname: firstNameInput.value,
+    lastname: lastNameInput.value,
+    age: ageInput.value,
+    genders: genderInput.value,
+    interests: interests,
+    description: descriptionInput.value,
+  };
+
+  console.log(userData);
+}
