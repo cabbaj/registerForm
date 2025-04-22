bfbac409d17858b1ad7c7c5c98d93a8de17cc25a
cabbaj <ppvadon@gmail.com>
2025-04-22 12:27:12 +0700

    crud

--- server/server.js
+++ server/server.js
@@ -11,9 +11,22 @@ let counter = 1;
 // define the format from body request (client send)
 app.use(bodyParser.json());
 
-// get user
+// get all user
 app.get("/users", (req, res) => {
-  res.json(users);
+  const fliterUsers = users.map((user) => {
+    return { id: user.id, firstname: user.firstname, lastname: user.lastname };
+  });
+
+  res.json(fliterUsers);
+});
+
+// get user by id
+app.get("/users/:id", (req, res) => {
+  let id = req.params.id;
+  // find index that user.id = id it will return index in array (index in array start at 0)
+  let selectIdx = users.findIndex((user) => user.id == id);
+
+  res.json(users[selectIdx]);
 });
 
 // create user
@@ -29,10 +42,36 @@ app.post("/user", (req, res) => {
   });
 });
 
-// update user with params
+// put for replace old data (that have data every field if not send data it will remove that field)
 app.put("/users/:id", (req, res) => {
   let id = req.params.id;
+  let updateUser = req.body;
+  let selectIdx = users.findIndex((user) => user.id == id);
 
+  // update
+  // user[selectIdx] is object you want to update 
+  // updateUser is what you want to update to object
+  Object.assign(users[selectIdx], updateUser);
+
+  res.json({
+    msg: "updated",
+    data: {
+      userUpdate: updateUser,
+      index: selectIdx,
+    },
+  });
+});
+
+// delete
+app.delete("/users/:id", (req, res) => {
+  let id = req.params.id;
+  let selectIdx = users.findIndex((user) => user.id == id);
+
+  users.splice(selectIdx, 1);
+
+  res.json({
+    msg: "deleted",
+  });
 });
 
 // connect to the server
